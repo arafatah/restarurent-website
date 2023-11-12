045a106c54069a09712eb68d2b7c8e8025424181
arafatah <arafathowlader456@gmail.com>
2023-11-12 16:40:45 +0600

    normal commit for first time

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Banner from "./Banner/Banner";
+import Category from "./Category/Category";
+import PopularMenu from "./PopularManu/PopularMenu";
+
+const Home = () => {
+    return (
+        <div>
+           <Banner/>
+           <Category/>
+           <PopularMenu/>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Pages/Home/PopularManu/PopularMenu.jsx
@@ -0,0 +1,14 @@
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+
+const PopularMenu = () => {
+    return (
+        <section>
+            <SectionTitle
+            heading={"From Our Menu"}
+            subHeading={"Popular Item"}
+            ></SectionTitle>
+        </section>
+    );
+};
+
+export default PopularMenu;
